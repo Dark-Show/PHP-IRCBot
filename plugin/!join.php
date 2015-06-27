80232f0555fcbced6b671ebcc16684f742668085
Gregory Michalik <gregory954@gmail.com>
2015-06-26 21:17:45 -0300

    formatting

--- plugin/!join.php
+++ plugin/!join.php
@@ -1,12 +1,14 @@
 <?php
 
 if($action=="privmsg"){
-
-if(substr($msg,0,5)=="!join"){
-$t=explode(" ",$msg, 3);
-if(isset($t[2])){IRC_Join($t[1],$t[2]);}else{IRC_Join($t[1]);}
-
-}
+  if(substr($msg,0,5)=="!join"){
+    $t=explode(" ",$msg, 3);
+    if(isset($t[2])){
+      IRC_Join($t[1],$t[2]);
+    }else{
+      IRC_Join($t[1]);
+    }
+  }
 }
 
 ?>
