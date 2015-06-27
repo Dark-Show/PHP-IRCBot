31b01579f640cb5cb9307465dcb696ddd06bee03
Gregory Michalik <gregory954@gmail.com>
2015-06-26 21:18:29 -0300

    Formatting

--- plugin/IRC_VERSION.php
+++ plugin/IRC_VERSION.php
@@ -1,1 +1,10 @@
-<?php if($action=="privmsg"){if(strtolower($msg)==Chr(1)."version".Chr(1)){IRC_PostNotice($usr, Chr(1)."VERSION ".$version.Chr(1));IRC_Rejoin($chan);}} ?>
+<?php
+
+if($action=="privmsg"){
+  if(strtolower($msg)==Chr(1)."version".Chr(1)){
+    IRC_PostNotice($usr, Chr(1)."VERSION ".$version.Chr(1));
+    IRC_Rejoin($chan);
+  }
+} 
+
+?>
