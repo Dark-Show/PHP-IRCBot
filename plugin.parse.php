f712fb1f6ce6ec14b72a974d5aa56778f38a0fa1
Gregory Michalik <gregory954@gmail.com>
2015-06-26 21:12:49 -0300

    Formatting

--- plugin.parse.php
+++ plugin.parse.php
@@ -1,14 +1,18 @@
 <?php
 
-if(is_dir($ROOT."plugin")){if($dh=opendir($ROOT."plugin")){
-        while(($file=readdir($dh))!==false){
-            if(is_file($ROOT."plugin/".$file)){
-		if(substr($file,-3)=="php"){
-			if(substr($file,0,3)!=="inc"){include($ROOT."plugin/".$file);}
-	   	}
-	    }
-	}
-closedir($dh);
-}}
+if(is_dir($ROOT."plugin")){
+	if($dh=opendir($ROOT."plugin")){
+        	while(($file=readdir($dh))!==false){
+            		if(is_file($ROOT."plugin/".$file)){
+				if(substr($file,-3)=="php"){
+					if(substr($file,0,3)!=="inc"){
+						include($ROOT."plugin/".$file);
+					}
+			   	}
+			 }
+		}
+		closedir($dh);
+	}	
+}
 
 ?>
